ed59fb35b3d3183ff5519af5ec8aa37ab0ead9ae
Julio Valle <juliovr_93@hotmail.com>
2018-05-15 17:44:02 -0500

    EmmitOn

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,11 @@
 package sample;
 
-import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXToggleButton;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import org.json.JSONObject;
 
 import java.net.URI;
@@ -14,53 +13,45 @@ import java.net.URI;
 public class Controller {
 
     @FXML
-    CheckBox CheckBoxOption1;
-    @FXML
-    JFXCheckBox CheckBoxOption2;
+    JFXToggleButton tglSwitch1,tglSwitch2;
 
     private Socket socket;
 
     public Controller () {
-        socket = IO.socket(URI.create("http://localhost:3000"));
+        socket = IO.socket(URI.create("https://juliosocketapp.herokuapp.com:443"));
         socket.on("server_checked", new Emitter.Listener() {
             @Override
             public void call(Object... objects) {
                 try {
                     JSONObject obj = (JSONObject)objects[0];
-                    System.out.println(obj);
-                    System.out.println("Checked: " + obj.getBoolean("checked_1"));
-                    System.out.println("Checked: " + obj.getBoolean("checked_2"));
-                    CheckBoxOption1.setSelected(obj.getBoolean("checked_1"));
-                    CheckBoxOption2.setSelected(obj.getBoolean("checked_2"));
+                    System.out.println("OnServerChecked: " + obj.toString());
+                    tglSwitch1.setSelected(obj.getBoolean("checked_1"));
+                    tglSwitch2.setSelected(obj.getBoolean("checked_2"));
                 } catch (Exception e) {
-                    System.out.println(e.toString());
+                    System.out.println("ErrorServerChecked: " + e.toString());
                 }
             }
         });
         socket.connect();
     }
 
-    public void onActionCheckBoxOption1 (ActionEvent event) {
-        try {
-            JSONObject obj = new JSONObject();
-            obj.put("checked_1", CheckBoxOption1.isSelected());
-            obj.put("checked_2", CheckBoxOption2.isSelected());
-            System.out.println("Object" + obj);
-            socket.emit("client_checked",obj);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
+    public void onActionToggleButtonSwitch1 (ActionEvent event) {
+        client_checked();
     }
 
-    public void onActionCheckBoxOption2 (ActionEvent event) {
+    public void onActionToggleButtonSwitch2 (ActionEvent event) {
+        client_checked();
+    }
+
+    private void client_checked () {
         try {
             JSONObject obj = new JSONObject();
-            obj.put("checked_1", CheckBoxOption1.isSelected());
-            obj.put("checked_2", CheckBoxOption2.isSelected());
-            System.out.println("Object" + obj);
+            obj.put("checked_1", tglSwitch1.isSelected());
+            obj.put("checked_2", tglSwitch2.isSelected());
             socket.emit("client_checked",obj);
+            System.out.println("EmmitClientChecked: " + obj.toString());
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println("ErrorClientChecked: " + e.toString());
         }
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -13,6 +16,13 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
         primaryStage.show();
     }
 
